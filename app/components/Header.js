d7c6147a039a40b7112e34c01118773d97ecf941
Arunima Chaudhuri <tinniarunima66@gmail.com>
2025-02-15 00:46:07 +0530

    first commit

---
+++ app/components/Header.js
@@ -0,0 +1,23 @@
+import { ethers } from "ethers"
+
+function Header({ account, setAccount }) {
+  async function connectHandler() {
+    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+    const account = ethers.getAddress(accounts[0])
+    setAccount(account);
+  }
+
+  return (
+    <header>
+      <p className="brand">coin.craze</p>
+
+      {account ? (
+        <button onClick={connectHandler} className="btn--fancy">[ {account.slice(0, 6) + '...' + account.slice(38, 42)} ]</button>
+      ) : (
+        <button onClick={connectHandler} className="btn--fancy">[ connect ]</button>
+      )}
+    </header>
+  );
+}
+
+export default Header;
---
+++ ignition/modules/Factory.js
@@ -0,0 +1,18 @@
+// This setup uses Hardhat Ignition to manage smart contract deployments.
+// Learn more about it at https://hardhat.org/ignition
+
+const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
+const { ethers } = require("hardhat");
+
+const FEE = ethers.parseUnits("0.01", 18)
+
+module.exports = buildModule("FactoryModule", (m) => {
+  // Get parameters
+  const fee = m.getParameter("fee", FEE);
+
+  // Define factory
+  const factory = m.contract("Factory", [fee]);
+
+  // Return factory
+  return { factory };
+})
